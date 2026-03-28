f2fa673746e3e29e56446926a7066bb6503609da
IslamCabarli <icabarli@std.beu.edu.az>
2026-03-28 22:30:47 +0400

    adding storage link

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
-    return view('welcome
-    
-    
-');
+    return view('welcome');
+});
+
+Route::get('/run-link', function () {
+    Artisan::call('storage:link --force');
+    return "Storage link created!";
 });
