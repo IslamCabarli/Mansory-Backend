37ece3abff88f78ac57782c11a77fa2e8ca6f587
IslamCabarli <icabarli@std.beu.edu.az>
2026-03-29 17:52:59 +0400

    fix: parse CLOUDINARY_URL when individual env vars aren't set

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,5 +1,21 @@
 <?php
 
+// Parse CLOUDINARY_URL if individual env vars aren't set
+$cloudinaryUrl = env('CLOUDINARY_URL');
+$cloudName = env('CLOUDINARY_CLOUD_NAME');
+$apiKey = env('CLOUDINARY_API_KEY');
+$apiSecret = env('CLOUDINARY_API_SECRET');
+
+// If individual vars aren't set, parse from URL
+if (!$cloudName && $cloudinaryUrl) {
+    // Format: cloudinary://api_key:api_secret@cloud_name
+    if (preg_match('/cloudinary:\/\/(.+?):(.+?)@(.+)/', $cloudinaryUrl, $matches)) {
+        $apiKey = $matches[1];
+        $apiSecret = $matches[2];
+        $cloudName = $matches[3];
+    }
+}
+
 return [
 
     /*
@@ -11,9 +27,9 @@ return [
     |
     */
 
-    'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-    'api_key' => env('CLOUDINARY_API_KEY'),
-    'api_secret' => env('CLOUDINARY_API_SECRET'),
+    'cloud_name' => $cloudName,
+    'api_key' => $apiKey,
+    'api_secret' => $apiSecret,
     'secure' => true,
 
 ];
